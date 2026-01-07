75319ace3cc17592f1ba821b58348c93e753537c
theliamladia <51178138+theliamladia@users.noreply.github.com>
2026-01-07 10:47:44 -0500

    Matrix theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'matrix': {
+          green: '#00ff41',
+          'dark-green': '#003b00',
+          'darker-green': '#001a00',
+          black: '#0d0208',
+        }
+      },
+      fontFamily: {
+        'matrix': ['DotGothic16', 'Courier New', 'monospace'],
+      }
+    },
   },
   plugins: [],
 }
